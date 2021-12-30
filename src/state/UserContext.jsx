4d10e136beec47342d4eaab2ac0fe97f5c930b91
Hanifrfd <50744311+hanifrafidi@users.noreply.github.com>
2021-12-30 16:41:10 +0700

    Update UserContext.jsx

--- src/state/UserContext.jsx
+++ src/state/UserContext.jsx
@@ -35,7 +35,7 @@ const UserProvider = (props) => {
 }
 
 const loginUser = (user,response,setUser,navigate) => {        
-    if(response.error != ''){
+    if(response.error != null){
         console.log(user)
         return console.log('error : ', response)
     }else{
